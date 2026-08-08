55239443211bd81db6b2bc9162801d8c57a1aa30
Mirzobaqo <mirzokarimovmirzobaqo@gmail.com>
2026-08-08 12:20:14 +0500

    first commit

---
+++ src/main/java/uz/uniSport/uni_sport/UniSportApplication.java
@@ -0,0 +1,13 @@
+package uz.uniSport.uni_sport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniSportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniSportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/uniSport/uni_sport/UniSportApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.uniSport.uni_sport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniSportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
